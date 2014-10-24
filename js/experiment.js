70c4fcc759a805a98c71d02f29bff9588c761165
dedcode <difallah@gmail.com>
2014-10-24 23:44:47 +0200

    fixing something

--- js/experiment.js
+++ js/experiment.js
@@ -2,13 +2,15 @@
 var Template = {}
 Template.message = Handlebars.compile($("#tmp").html());
 var id = gup("id");
-url = "https://api.mongolab.com/api/1/databases/hitclassify/collections/hit?q={id:"+ id + "}&apiKey=FzmG9iesxbf045DUjY1tfo65U7584rWO"
-$.ajax( { url: url,
-          type: "GET",
-          contentType: "application/json",
-          success: function(data) {
-                   console.log(url);
-                   console.log(data);
-                   $('#container-task').html(Template.message(data[0]));
-                }
-      } );
+if(id!=="") {
+    url = "https://api.mongolab.com/api/1/databases/hitclassify/collections/hit?q={id:"+ id + "}&apiKey=FzmG9iesxbf045DUjY1tfo65U7584rWO"
+    $.ajax( { url: url,
+              type: "GET",
+              contentType: "application/json",
+              success: function(data) {
+                       console.log(url);
+                       console.log(data);
+                       $('#container-task').html(Template.message(data[0]));
+                    }
+          } );
+}
